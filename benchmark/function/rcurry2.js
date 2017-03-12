04d90ed06647bef846ca5132d8f8ba0ade1abaf0
valtermro <valtermro@outlook.com>
2017-03-12 12:26:00 -0300

    (fix bench) Fixes imports in benchmarks for rcurry

--- benchmark/function/rcurry2.js
+++ benchmark/function/rcurry2.js
@@ -1,7 +1,7 @@
-require('../../_dev/babel.register');
+require('../../build/babel.register');
 const Benchmark = require('Benchmark');
 const suite = new Benchmark.Suite('function.rcurry() arity == 2');
-const util = require('../../_dev/util');
+const util = require('../../build/util');
 
 const rcurry = require('../../src/function/rcurry');
 const { curryRight } = require('lodash/fp');
